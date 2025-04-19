c2bcfdbdc5c8495c57a6194082aad4a8e756cd69
Olga <minaeva9@gmail.com>
2025-04-19 15:52:45 -0400

    Button added

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,3 +1,5 @@
+import Button from "../ui/Button";
+
 function AddTask() {
   return (
     <form className="flex flex-col sm:flex-row justify-center items-center gap-2 p-4 max-w-2xl mx-auto">
@@ -12,12 +14,7 @@ function AddTask() {
         maxLength={25}
         className="w-full sm:w-auto flex-1 border-amber-500 rounded-md p-4 border-2 focus:outline-none focus:ring-1 focus:ring-red-400 dark:text-white dark:bg-zinc-800"
       />
-      <button
-        type="submit"
-        className="px-6 py-4 border-2 border-amber-500 rounded-md bg-amber-400 text-md hover:bg-amber-500 transition transform hover:scale-105 active:scale-95 dark:text-black"
-      >
-        Add
-      </button>
+    <Button>Add</Button>
     </form>
   );
 }
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,12 @@
-function Button() {
+function Button({children}) {
  return (
-  <button>
-   
-  </button>
- )
+   <button
+     type="submit"
+     className="px-6 py-4 border-2 border-amber-500 rounded-md bg-amber-400 text-md hover:bg-amber-500 transition transform hover:scale-105 active:scale-95 dark:text-black"
+   >
+     {children}
+   </button>
+ );
 }
 
 export default Button
